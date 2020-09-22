ce30117657d3b1cbe5162d60e9d311116c8ef32b
Andrew Ruiz <nukedoom@gmail.com>
2020-09-22 15:27:15 -0700

    Adding spacing to drop down menus

--- app/src/components/recommends/newRecommend.jsx
+++ app/src/components/recommends/newRecommend.jsx
@@ -8,6 +8,24 @@ import Button from '@material-ui/core/Button'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    marginLeft: '0.8rem',
+    marginRight: '0.8rem',
+    marginTop: '2rem'
+  },
+}));
+
 
 
 const NewRecommend = (props) => {
@@ -55,87 +73,132 @@ const NewRecommend = (props) => {
     console.log(recommendToEdit);
   };
 
-  return (
-    <div className="newRecommend">
-      <h3>How do you feel today?</h3>
-      <form onSubmit={addNewRecommend}>
-        <label>
-          <TextField>
-          <input
-            type="text"
-            name="title"
-            value={recommendToEdit.title}
-            onChange={onChangeHandler}
-            placeholder="title"
-          />
-          </TextField>
-        </label>
-        <label>
-          Favorite Flavor:
-          <Select value={recommendToEdit.flavor} onChange={onChangeHandler}>
-            <MenuItem value="grapefruit">Grapefruit</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="mango">Mango</MenuItem>
-          </Select>
-        </label>
-        <label>
-          Type:
-          <Select
-            name="types"
-            value={recommendToEdit.types}
-            onChange={onChangeHandler}
-          >
-            <MenuItem value="">types</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="jello">jello</MenuItem>
-          </Select>
-        </label>
-        <label>
-          Ailment:
-          <Select value={recommendToEdit.ailments} onChange={onChangeHandler}>
-            <MenuItem value="ailments">Grapefruit</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="mango">Mango</MenuItem>
-          </Select>
-        </label>
-        <label>
-          Effects:
-          <Select value={recommendToEdit.effects1} onChange={onChangeHandler}>
-            <MenuItem value="effects">Grapefruit</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="mango">Mango</MenuItem>
-          </Select>
-        </label>
-        <label>
-          Effects:
-          <Select value={recommendToEdit.effects2} onChange={onChangeHandler}>
-            <MenuItem value="effects">Grapefruit</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="mango">Mango</MenuItem>
-          </Select>
-        </label>
-        <label>
-          Effects:
-          <Select value={recommendToEdit.effects3} onChange={onChangeHandler}>
-            <MenuItem value="effects">Grapefruit</MenuItem>
-            <MenuItem value="grapefruit">Grapefruit</MenuItem>
-            <MenuItem value="lime">Lime</MenuItem>
-            <MenuItem value="coconut">Coconut</MenuItem>
-            <MenuItem value="mango">Mango</MenuItem>
-          </Select>
-        </label>
+  const classes = useStyles();
 
-        <Button variant="contained" color="primary" id="addButton" type="submit">
-          Add Recommend
-        </Button>
-      </form>
-    </div>
+  return (
+
+    <div className="newRecommend">
+    <h3>How do you feel today?</h3>
+    <form onSubmit={addNewRecommend}>
+
+    <div className='menuItemContainer'>
+    <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        <TextField>
+        <input
+          type="text"
+          name="title"
+          value={recommendToEdit.title}
+          onChange={onChangeHandler}
+          placeholder="title"
+        />
+        </TextField>
+      </label>
+        </Paper>
+      </Grid>
+      
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        Favorite Flavor:
+        <Select value={recommendToEdit.flavor} onChange={onChangeHandler}>
+          <MenuItem value="grapefruit">Grapefruit</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="mango">Mango</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        Type:
+        <Select
+          name="types"
+          value={recommendToEdit.types}
+          onChange={onChangeHandler}
+        >
+          <MenuItem value="">types</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="jello">jello</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+      <label>
+        Ailment:
+        <Select value={recommendToEdit.ailments} onChange={onChangeHandler}>
+          <MenuItem value="ailments">Grapefruit</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="mango">Mango</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        Effects:
+        <Select value={recommendToEdit.effects1} onChange={onChangeHandler}>
+          <MenuItem value="effects">Grapefruit</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="mango">Mango</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        Effects:
+        <Select value={recommendToEdit.effects2} onChange={onChangeHandler}>
+          <MenuItem value="effects">Grapefruit</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="mango">Mango</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+        <label>
+        Effects:
+        <Select value={recommendToEdit.effects3} onChange={onChangeHandler}>
+          <MenuItem value="effects">Grapefruit</MenuItem>
+          <MenuItem value="grapefruit">Grapefruit</MenuItem>
+          <MenuItem value="lime">Lime</MenuItem>
+          <MenuItem value="coconut">Coconut</MenuItem>
+          <MenuItem value="mango">Mango</MenuItem>
+        </Select>
+      </label>
+        </Paper>
+      </Grid>
+      </div>
+
+      <Button variant="contained" color="primary" id="addButton" type="submit">
+        Add Recommend
+      </Button>
+
+    </form>
+
+  </div>
   );
+  
 };
 
 export default NewRecommend;
